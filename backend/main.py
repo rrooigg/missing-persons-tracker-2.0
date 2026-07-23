fc0ec2f6b0ca14f5968853044d4e1018097b478e
rrooigg <devnadifa@gmail.com>
2026-07-23 11:14:27 +0300

    Fix CORS origin

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["http://localhost:5173", "https://missing-persons-tracker.vercel.app/"],
+  allow_origins=["http://localhost:5173", "https://missing-persons-tracker.vercel.app"],
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
