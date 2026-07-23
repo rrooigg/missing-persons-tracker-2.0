4dc85885f343893c2aa6d9b70a3b4af2fc181f0c
rrooigg <devnadifa@gmail.com>
2026-07-23 08:59:21 +0300

    Use environment variable for database

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,9 @@ from sqlalchemy import create_engine   #core object to communicate with database
 from sqlalchemy.ext.declarative import declarative_base  #creates a base class which db models will inherit from
 from sqlalchemy.orm import sessionmaker   #session like a 'conversation' with the db i.e crud operations with db
 #database url
-DATABASE_URL = "postgresql://postgres:Abdigani04@localhost/prisoners"
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 #create engine -> create 1 engine for whole application(manages connections, sends sql queries, talk to postresql)
 engine = create_engine(DATABASE_URL)
