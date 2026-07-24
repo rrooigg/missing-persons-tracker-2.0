7c5601d758085241ee3146e30fcfe298204c739c
rrooigg <devnadifa@gmail.com>
2026-07-24 11:14:54 +0300

    use models that use less RAM to delploy

--- backend/face_recognition.py
+++ backend/face_recognition.py
@@ -5,9 +5,9 @@ def verify_faces(uploaded_image_path, stored_image_path):
   result = DeepFace.verify(
     img1_path=uploaded_image_path,
     img2_path=stored_image_path,
-    model_name="ArcFace",
-    detector_backend="retinaface",
-    enforce_detection=True
+    model_name="Facenet",
+    detector_backend="opencv",
+    enforce_detection=False
   )
   return (
     result["verified"],
