bbdd36a81a13594711e10eeedf357cd08572a632
rrooigg <devnadifa@gmail.com>
2026-07-24 11:29:45 +0300

    Adding a garbage collection trigger right after the verification step to immediately free up memory so function can finish returning the data to the frontend.

--- backend/face_recognition.py
+++ backend/face_recognition.py
@@ -1,16 +1,21 @@
 from deepface import DeepFace
-
+import tensorflow as tf
+import gc
 
 def verify_faces(uploaded_image_path, stored_image_path):
-  result = DeepFace.verify(
-    img1_path=uploaded_image_path,
-    img2_path=stored_image_path,
-    model_name="Facenet",
-    detector_backend="opencv",
-    enforce_detection=False
-  )
-  return (
-    result["verified"],
-    result["distance"],
-    result["threshold"]
-  )
+  try:
+    result = DeepFace.verify(
+      img1_path=uploaded_image_path,
+      img2_path=stored_image_path,
+      model_name="Facenet",
+      detector_backend="opencv",
+      enforce_detection=False
+    )
+    return (
+      result["verified"],
+      result["distance"],
+      result["threshold"]
+    )
+  finally:
+    tf.keras.backend.clear_session()
+    gc.collect()
