3b3eb67d3ba258b77c61f0ee268950e13c93b58a
rrooigg <devnadifa@gmail.com>
2026-07-24 10:06:10 +0300

    remove degugging code

--- backend/database.py
+++ backend/database.py
@@ -8,10 +8,7 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 #create engine -> create 1 engine for whole application(manages connections, sends sql queries, talk to postresql)
-engine = create_engine(DATABASE_URL,
-                      pool_pre_ping=True,
-                      pool_recycle=300,
-                       )
+engine = create_engine(DATABASE_URL)
 
 #create sessions
 SessionLocal = sessionmaker(
